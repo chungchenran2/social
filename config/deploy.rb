e7c5ea3643c2b428df1a2216e46d188e0e43f574
chungchenran2 <chungchen.ran@sjsu.edu>
2016-12-25 10:24:24 -0800

    Removed namespace etc in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,15 +32,15 @@ set :deploy_to, '/var/www/social'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
-	desc 'Restart application'
-	task :restart do
-		on roles(:app), in: :sequence, wait: 5 do
+#namespace :deploy do
+	#desc 'Restart application'
+	#task :restart do
+		#on roles(:app), in: :sequence, wait: 5 do
 			# Your restart mechanism here, for example:
-			execute :touch, release_path.join('tmp/restart.txt')
-		end
-	end
+			#execute :touch, release_path.join('tmp/restart.txt')
+		#end
+	#end
 
-	after :publishing, :restart
+	#after :publishing, :restart
 
-end
+#end
