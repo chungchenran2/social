4fd0eb63e01c6522ec11c144600739a3cd9453cc
chungchenran2 <chungchen.ran@sjsu.edu>
2016-12-25 10:13:18 -0800

    Correct File config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock "3.7.1"
 
-set :application, "social"
-set :repo_url, "https://github.com/chungchenran2/social.git"
+set :application, 'social'
+set :repo_url, 'https://github.com/chungchenran2/social.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/social"
+set :deploy_to, '/var/www/social'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -31,3 +31,16 @@ set :deploy_to, "/var/www/social"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+namespace :deploy do
+	desc 'Restart application'
+	task :restart do
+		on roles(:app), in: :sequence, wait: 5 do
+			# Your restart mechanism here, for example:
+			execute :touch, release_path.join('tmp/restart.txt')
+		end
+	end
+
+	after :publishing, :restart
+
+end
